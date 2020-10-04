e6e90a4b580ec3f0a0a618805577c48c0c407497
Łukasz Korcz <lukaszwp55@gmail.com>
2020-10-04 19:54:37 +0200

    create: room validator

--- app/routes/authRoute.js
+++ app/routes/authRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
 import { login } from '../controllers/authController';
+
 import { loginValid } from '../helpers/validators/authValidator';
 
 const router = express.Router();
 
-router.post('/auth/login', loginValid, login);
+router.post('/auth/login',
+  loginValid,
+  login);
 
 export default router;
--- app/routes/roomsRoute.js
+++ app/routes/roomsRoute.js
@@ -4,6 +4,7 @@ import {
   setUser,
   verifyToken,
 } from '../middlewares/verifyAuth';
+
 import {
   createRoom,
   deleteRoom,
@@ -12,12 +13,28 @@ import {
   updateRoom,
 } from '../controllers/roomsController';
 
+import { createRoomValid } from '../helpers/validators/roomsValidator';
+
 const router = express.Router();
 
-router.get('/rooms/:id', getRoom);
-router.delete('/rooms/:id', verifyToken, setUser, deleteRoom);
 router.get('/rooms/', getRooms);
-router.post('/rooms/', verifyToken, setUser, createRoom);
-router.patch('/rooms/:id', verifyToken, setUser, updateRoom);
+
+router.get('/rooms/:id', getRoom);
+
+router.post('/rooms/',
+  createRoomValid,
+  verifyToken,
+  setUser,
+  createRoom);
+
+router.patch('/rooms/:id',
+  verifyToken,
+  setUser,
+  updateRoom);
+
+router.delete('/rooms/:id',
+  verifyToken,
+  setUser,
+  deleteRoom);
 
 export default router;
