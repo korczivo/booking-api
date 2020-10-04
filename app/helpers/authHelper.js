3713e476bcc82af77df42c3039372b5310820dac
Łukasz Korcz <lukaszwp55@gmail.com>
2020-10-04 19:58:37 +0200

    remove: unused code

---
+++ app/helpers/authHelper.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+import env from '../../env';
+
+/**
+ * Generate token
+ * @param {string} id
+ * @returns {string} token
+ * */
+
+const generateToken = id => {
+  const token = jwt.sign({
+    user_id: id,
+  },
+  env.secret, { expiresIn: '3d' });
+
+  return token;
+};
+
+/**
+ * Hash password method
+ * @param {string} password
+ * @returns {string} return hashed password
+* */
+const saltRound = 10;
+const salt = bcrypt.genSaltSync(saltRound);
+const hashPassword = password => bcrypt.hashSync(password, salt);
+
+/**
+ * Compare password method
+ * @param {string} password
+ * @param {string} hashedPassword
+ * @returns {string} return boolean
+ * */
+
+const comparePassword = (hashedPassword, password) => bcrypt.compareSync(password, hashedPassword);
+
+export {
+  comparePassword,
+  generateToken,
+  hashPassword,
+};
