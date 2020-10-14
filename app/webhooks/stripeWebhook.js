b3e388d2f9ffcb0132bb56566688e2bdd63d73c5
Łukasz Korcz <lukaszwp55@gmail.com>
2020-10-14 18:12:19 +0200

    initial setup stripe webhook

---
+++ app/webhooks/stripeWebhook.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import stripe from 'stripe';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  let eventType;
+
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    const signature = req.headers['stripe-signature'];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log('⚠️  Webhook signature verification failed.');
+
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    const { data } = event;
+
+    eventType = event.type;
+    console.log(eventType);
+
+    if (eventType === 'checkout.session.succeeded') {
+      console.log('🔔  Payment received!');
+      console.log(data);
+      const session = data.object;
+
+      console.log('handle payment succeeded');
+    } else if (eventType === 'account.updated') {
+      console.log('🔔  Stripe Account updated!');
+      console.log(data);
+      const account = data.object;
+
+      console.log('handle account update');
+    }
+  }
+  res.sendStatus(200);
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import '@babel/polyfill';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import env from './env';
 import usersRoute from './app/routes/usersRoute';
 import authRoute from './app/routes/authRoute';
@@ -8,14 +9,27 @@ import roomsRoute from './app/routes/roomsRoute';
 import commentsRoute from './app/routes/commentsRoute';
 import reservationRoute from './app/routes/reservation';
 import paymentRoute from './app/routes/paymentRoute';
+import stripeWebhook from './app/webhooks/stripeWebhook';
 
 const app = express();
 
 // Add middleware for parsing URL encoded bodies (which are usually sent by browser)
 app.use(cors());
+// app.use(bodyParser.json());
 // Add middleware for parsing JSON and urlencoded data and populating `req.body`
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// app.use(
+//   express.json({
+//     // We need the raw body to verify webhook signatures.
+//     // Let's compute it only when hitting the Stripe webhook endpoint.
+//     verify(req, res, buf) {
+//       if (req.originalUrl.startsWith('/api/v1/webhook')) {
+//         req.rawBody = buf.toString();
+//       }
+//     },
+//   })
+// );
 
 app.use('/api/v1', usersRoute);
 app.use('/api/v1', authRoute);
@@ -23,6 +37,7 @@ app.use('/api/v1', roomsRoute);
 app.use('/api/v1', commentsRoute);
 app.use('/api/v1', reservationRoute);
 app.use('/api/v1', paymentRoute);
+app.use('/webhook/stripe', bodyParser.raw({ type: '*/*' }), stripeWebhook);
 
 app.listen(env.port).on('listening', () => {
   console.log(`🚀 are live on ${env.port}`);
