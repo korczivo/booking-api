16bfe65404dcb523909995efba7af61e492c89a5
Łukasz Korcz <lukaszwp55@gmail.com>
2020-10-24 12:19:14 +0200

    create payment indent service, fix serve, payment working

---
+++ app/services/paymentServices.js
@@ -0,0 +1,30 @@
+import stripe from 'stripe';
+import env from '../../env';
+import { status } from '../helpers/status';
+import dbQuery from '../db/dev/dbQuery';
+
+const stripeApi = stripe(env.stripe_secret_key);
+
+export const paymentService = async reservation_id => {
+  const getReservationQuery = 'SELECT * FROM reservations WHERE id=$1';
+
+  try {
+    const { rows } = await dbQuery.query(getReservationQuery, [reservation_id]);
+    const dbResponse = rows[0];
+
+    const paymentIntent = await stripeApi.paymentIntents.create({
+      amount: dbResponse.reservation_price,
+      currency: 'usd',
+    });
+
+    return {
+      response: {
+        clientSecret: paymentIntent.client_secret,
+        publishableKey: env.stripe_public_key,
+      },
+      status: status.success,
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
